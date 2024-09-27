75361cc3d01de4a20fdec401f2a31289658dbf67
oliebueno <ollibueno08@gmail.com>
2024-09-27 11:50:31 -0400

    AGREGADO: prueba 1

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,7 +1,7 @@
 // Endpoint for querying the fibonacci numbers
 
 import { Request, Response } from 'express';
-const fibonacci = require("./fib");
+const fibonacci: (n: number) => number = require("./fib");
 
 export default (req: Request, res: Response) => {
   const { num } = req.params;
